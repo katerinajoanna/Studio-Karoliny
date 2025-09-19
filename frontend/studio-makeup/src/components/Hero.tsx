ca05350d37b6513fc00afeb7fc38ad4d2a8d2e94
Katarzyna Potarowicz <yhfe23.potaka@folkuniversitetet.nu>
2025-09-19 13:57:11 +0200

    removed commented-out sections

--- frontend/studio-makeup/src/components/Hero.tsx
+++ frontend/studio-makeup/src/components/Hero.tsx
@@ -1,32 +1,3 @@
-
-// const Hero: React.FC = () => {
-//     return (
-//         <section className="relative flex flex-col md:flex-row items-center justify-evenly font-malayalam p-6 md:p-12 overflow-hidden shadow-lg">
-//             <div className="text-generalText mt-10 md:mt-0 md:ml-12 max-w-xl animate-fadeIn p-5 md:p-7 lg:p-8 xl:p-9">
-//                 <h1 className="font-bold text-3xl sm:text-4xl md:text-5xl p-2 md:p-3">Witaj!</h1>
-//                 <div className="font-medium text-textPrimary text-base sm:text-lg md:text-xl space-y-3">
-//                     <p>Szukasz profesjonalnej wizażystki, stylistki brwi lub linergistki?</p>
-//                     <p>Zapraszam Cię do skorzystania z moich usług w Pomlewie i Gdańsku Kokoszkach. Istnieje także możliwość dojazdu - po wcześniejszej konsultacji.</p>
-//                     <p>Jestem dyplomowaną wizażystką i stylistką brwi z wieloletnim doświadczeniem. Pracuję z pasją, precyzją i na wysokiej jakości kosmetykach.</p>
-//                 </div>
-//             </div>
-
-//             <img
-//                 className="w-full max-w-[300px] md:max-w-[350px] h-auto object-contain mt-6 md:mt-0 animate-fadeIn"
-//                 src="/karolina.png"
-//                 alt="Zdjęcie"
-//             />
-//         </section>
-
-
-
-//     );
-// };
-
-// export default Hero;
-
-
-
 const Hero: React.FC = () => {
     return (
         <section className="mx-auto my-10 md:my-20 lg:my-30  p-5 shadow-lg">
--- frontend/studio-makeup/src/pages/HomePage.tsx
+++ frontend/studio-makeup/src/pages/HomePage.tsx
@@ -1,35 +1,3 @@
-// import About from "../components/About";
-// import Contact from "../components/contact/Contact";
-// import Footer from "../components/Footer";
-// import Gallery from "../components/Gallery";
-// import Header from "../components/Header";
-// import Hero from "../components/Hero";
-// import Nav from "../components/Nav";
-// import Offer from "../components/offer/Offer";
-
-// const HomePage: React.FC = () => {
-//     return (
-//         <div
-//             className="min-h-screen flex flex-col bg-cover bg-center"
-//             style={{ backgroundImage: "url('/background4.png')" }}
-//         >
-//             <Header />
-//             <Nav />
-//             <Hero />
-//             <Offer />
-//             <Gallery />
-//             <About />
-//             <Contact />
-//             <Footer />
-//         </div>
-//     );
-// };
-
-// export default HomePage;
-
-
-
-// backroud-attachmet
 import About from "../components/About";
 import BackToTop from "../components/buttons/backToTop";
 import Contact from "../components/contact/Contact";
